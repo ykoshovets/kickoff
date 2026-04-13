4be26b31d3cb18cab7a98e2899e5db424c2ef190
Yevhenii Koshovets <y.koshovets@gmail.com>
2026-04-13 03:56:27 +0300

    add match-service skeleton, update gitignore

---
+++ match-service/src/main/java/com/kickoff/match_service/MatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kickoff.match_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchServiceApplication.class, args);
+	}
+
+}
